a9403bd86f1eb7b86e4e11117313c7de8ce7f99e
Mark Mims <mmm@digitalocean.com>
2019-02-07 13:56:18 -0700

    adds do/managed-k8s layer to install do managed k8s cluster

---
+++ terraform/layers/do/managed-k8s/io.tf
@@ -0,0 +1,11 @@
+
+variable "project" {}
+variable "environment" {}
+
+variable "region" {}
+variable "state_bucket" {}
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "digitalocean_token" {}
+
