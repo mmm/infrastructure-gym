8ab7aaf9c6a6e2fc8d051e64a32dd5b278208ed6
Mark Mims <mmm@digitalocean.com>
2019-05-06 09:41:02 -0600

    pulling ansible tarball

--- terraform/layers/gcp/support/main.tf
+++ terraform/layers/gcp/support/main.tf
@@ -14,18 +14,56 @@ data "terraform_remote_state" "core" {
   }
 }
 
-module "bastion" {
+#module "bastion" {
+  #source = "../../../modules/gcp/generic_instance"
+  #name = "${var.project}-${var.environment}-bastion"
+  ##machine_type = "n1-standard-1"
+
+  #ssh_keys = ["${var.ssh_keys}"]
+  #ansible_tarball = {
+    #project            = "${var.project}"
+    #environment        = "${var.environment}"
+    #region             = "${var.region}"
+    #bucket             = "${var.state_bucket}"
+    #vault_password     = "${var.ansible_vault_password}"
+    #role               = "ugh"
+  #}
+#}
+
+module "bastions" {
   source = "../../../modules/gcp/generic_instance"
   name = "${var.project}-${var.environment}-bastion"
-  #machine_type = "n1-standard-1"
-
+  count = 1 
+  region = "${var.region}"
   ssh_keys = ["${var.ssh_keys}"]
+  tags = [
+    "${var.project}-${var.environment}-ansible-role-bastion",
+  ]
   ansible_tarball = {
     project            = "${var.project}"
     environment        = "${var.environment}"
     region             = "${var.region}"
     bucket             = "${var.state_bucket}"
     vault_password     = "${var.ansible_vault_password}"
-    role               = "ugh"
+    role               = "${var.project}-${var.environment}-ansible-role-bastion"
+  }
+}
+
+module "consul_servers" {
+  source = "../../../modules/gcp/generic_instance"
+  name = "${var.project}-${var.environment}-consul"
+  count = 1
+  region = "${var.region}"
+  ssh_keys = ["${var.ssh_keys}"]
+  tags = [
+    "${var.project}-${var.environment}-ansible-role-consul-server",
+  ]
+  ansible_tarball = {
+    project = "${var.project}"
+    environment = "${var.environment}"
+    region = "${var.region}"
+    bucket = "${var.state_bucket}"
+    vault_password = "${var.ansible_vault_password}"
+    role               = "${var.project}-${var.environment}-ansible-role-consul-server"
   }
 }
